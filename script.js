ac99c68b90fb93ae94e68ae76cb3065448d75d55
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-12-07 00:07:05 -0500

    Task 1.1 complete

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 // Select and cache the <main>element in a variable named mainEl.
 mainEl = document.querySelector('main')
 
+// Task 1.1
+// Set the background color of mainElto the value stored in the --main-bgCSS custom property.
+
+// Hint: Assign a string that uses the CSS var()function like this:
+// 'var(--main-bg)'
+
+mainEl.style.backgroundColor = 'var(--main-bg)'
